fd192dbedc0efc0ff709d448ea122afef4ff8d7a
Etagegn <ewehabe@gmail.com>
2022-03-21 11:35:50 -0700

    My commit message

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
 /**
  * _strlen - function that returns the length of a string
  *
- *@s: length of a string.
+ * @s: length of a string.
  *
  * Return: 0.
  */
 
 int _strlen(char *s)
 {
-int i - 0;
+int i = 0;
 
 while (s[i] != 0)
 {
